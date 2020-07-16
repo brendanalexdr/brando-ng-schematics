8fdf153cf55e6ea59459611ee322234dafc3b5e8
nukuuk <nukuuk@gmail.com>
2020-07-15 23:51:24 -0500

    misc:  havent updated for while

--- src/init-brando-scss/index.ts
+++ src/init-brando-scss/index.ts
@@ -2,6 +2,7 @@ import {
   MergeStrategy,
   Rule,
   SchematicContext,
+  SchematicsException,
   Tree,
   apply,
   chain,
@@ -15,12 +16,25 @@ import {
   addPackageJsonDependency,
 } from "schematics-utilities";
 
+import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
 import { strings } from "@angular-devkit/core/src/utils";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function initBrandoScss(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const workspaceConfigBuffer = tree.read("angular.json");
+    if (!workspaceConfigBuffer) {
+      throw new SchematicsException("Not an Angular CLI workspace");
+    }
+    // const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+    // const projectName: string = _options.project || workspaceConfig.defaultProject;
+    // const project = workspaceConfig.projects[projectName];
+
+    // const defaultProjectPath = buildDefaultPath(project);
+    // const parsedPath = parseName(defaultProjectPath, _options.name);
+    // const { name, path } = parsedPath;
+
     if (tree.exists("src/styles.scss")) {
       tree.delete("src/styles.scss");
     }
@@ -34,6 +48,12 @@ export function initBrandoScss(_options: any): Rule {
       version: "^11.0.0",
       overwrite: true,
     });
+    _context.addTask(
+      new NodePackageInstallTask({
+        workingDirectory: _options.directory,
+        packageManager: _options.packageManager,
+      })
+    );
 
     return chain([
       mergeWith(add0ModulesFolder(), MergeStrategy.Overwrite),
